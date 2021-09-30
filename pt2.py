b8ec33b341764fafffa1ce86cd16b50033247502
winxter17 <sharady2a@gmail.com>
2021-09-30 17:26:04 +0530

    com2

---
+++ pt2.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number here: "))
+b=int(input("Enter second number here: "))
+def avg(a,b):
+    average= (a+b)/2
+    print(average)
+    return average
+print("This is the average of the given 2 numbers:")
+avg(a,b)
+
